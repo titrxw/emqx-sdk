b7b21dcc04c4932b8595232b05d9f257abff72bc
titrxw <1120309488@qq.com>
2021-11-30 22:25:05 +0800

    update

--- src/OpenApi/Mqtt/Message.go
+++ src/OpenApi/Mqtt/Message.go
@@ -9,7 +9,7 @@ type Message struct {
 	kernel.OpenApiAbstract
 }
 
-func (this *Message) Publish(ctx context.Context, topic string, clientId string, payload string, qos int, retain bool) error {
+func (this *Message) Publish(ctx context.Context, clientId string, topic string, payload string, qos int, retain bool) error {
 	_, err := this.Client.Post(ctx, "api/v4/mqtt/publish", map[string]interface{}{
 		"topic":    topic,
 		"clientid": clientId,
